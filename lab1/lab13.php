45bdf827727f3ad0ad6a77549ab89334bcc66981
EduardDevBD <lorenzoeduard789@gmail.com>
2022-09-01 12:58:45 -0500

    Agregando el lab13

---
+++ lab1/lab13.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorio 1.3</title>
+</head>
+<body>
+    <center>
+        <?php
+        //Imprimir una funcion propia de PHP que muestra info actual del lenguaje
+            echo phpinfo();
+        ?>
+    </center>
+</body>
+</html>
