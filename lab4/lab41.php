33763dc82944db19059ce4880f3399a078775c9d
EduardDevBD <lorenzoeduard789@gmail.com>
2022-09-08 18:22:12 -0500

    Modificando la ruta de las imagenes en lab41php

--- lab4/lab41.php
+++ lab4/lab41.php
@@ -1,12 +1,12 @@
 <?php
     $porcentaje = $_POST['porce'];
     if ($porcentaje >= 80 or $porcentaje == 100 ) {
-        echo "<img src='capture.png' alt='error'>";
+        echo "<img src='img/capture.png' alt='error'>";
     }
     elseif ($porcentaje >= 50 or $porcentaje == 79 ) {
-        echo "<img src='less.png' alt='error'>";
+        echo "<img src='img/less.png' alt='error'>";
     }
     elseif ($porcentaje <= 49 or $porcentaje >= 1){
-        echo "<img src='sad.png' alt='error'>";
+        echo "<img src='img/sad.png' alt='error'>";
     }
 ?>
