402a960c2107c44af2a0c13634ca74612c1fad31
EduardDevBD <lorenzoeduard789@gmail.com>
2022-09-08 20:18:04 -0500

    Editando el lab44php

--- lab4/lab44.php
+++ lab4/lab44.php
@@ -3,21 +3,21 @@ $numero = $_POST['num'];
 $TAM = 10;
 if ($numero%2==0) {
         for ($i=0; $i <= $TAM; $i++) {
-            echo "
+            if ($numero%2==0) {
+                echo $numero;
+            }
+            /*echo "
             <form>
             <fieldset>
-                <legend>Hola</legend>
+                <legend>$i</legend>
             </fieldset>
             </form>
-            ";
+            ";*/
         }
 }else{
     echo "Introduce un valor par, por favor.";
     header("lab44.html");
 }
-
-
-{ ?> <p>Aqui es un php</p> <?php }
 
 ?>
 <a href="lab44.html">Volver</a>
