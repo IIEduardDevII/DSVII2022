bff8dc6563e04234e87d2200b7008165d24df30f
EduardDevBD <lorenzoeduard789@gmail.com>
2022-09-22 18:32:56 -0500

    Agregando todos los archivos del lab6

---
+++ lab6/lab61.php
@@ -0,0 +1,36 @@
+<?php
+
+class cliente {
+
+    var $nombre;
+    var $numero;
+    var $peliculas_alquiladas;
+
+    function __construct($nombre, $numero)
+        {
+        $this->nombre=$nombre;
+        $this->numero=$numero;
+        $this->peliculas_alquiladas=array();
+        }
+
+    function __destruct()
+    {
+        echo "<br> destruido".$this->nombre;
+    }
+
+    function dame_numero()
+    {
+        return $this->numero;
+    }
+}
+
+//instaciamos un par de objetos cliente
+$cliente1 = new cliente("pepe", 1);
+$cliente2 = new cliente("Roberto", 564);
+
+//mostremos el numero de cada cliente creado
+echo "<br> El identificador del cliente 1 es: ".$cliente1->dame_numero();
+echo "<br> El identificador del cliente 2 es: ".$cliente2->dame_numero();
+
+
+?>
