c17399c2f2c3ecb76de515bfc92743766f0d2399
EduardDevBD <lorenzoeduard789@gmail.com>
2022-10-30 20:06:32 -0500

    Agregando algunos cambios al archivo

--- lab13/lab133.php
+++ lab13/lab133.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laboratorio 13</title>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
 </head>
 <body>
     <h1>Recuperar valor de una Cookie</h1>
@@ -16,6 +17,6 @@
            "<h2>Bienvenido Invitado!</h2><br/>";
     ?>
     <a href="lab131.php">...Regresar</a>$nbsp;
-    <a>Continuar...</a>
+    <a href="lab134.php">Continuar...</a>
 </body>
 </html>
